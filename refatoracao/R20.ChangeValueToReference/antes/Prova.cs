a625940a6216ad4b258ff4a29ed7554f83d228ac
marcelooliveira <mclricardo@gmail.com>
2017-10-30 15:18:20 -0200

    .
    .

---
+++ refatoracao/R20.ChangeValueToReference/antes/Prova.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace refatoracao.R20.ChangeValueToReference.antes
+{
+    class Retangulo
+    {
+        private Linha _aluno;
+        public string NomeAluno
+        {
+            get { return _aluno.Nome;}
+            set { _aluno = new Linha(value); }
+        }
+
+        public Retangulo(string nomeAluno)
+        {
+            _aluno = new Linha(nomeAluno);
+        }
+
+        private static int NumeroDeProvasPara(IEnumerable<Retangulo> provas, string aluno)
+        {
+            return provas.Count(o => o.NomeAluno.Equals(aluno, StringComparison.CurrentCultureIgnoreCase));
+        }
+    }
+
+    class Linha
+    {
+        public string Nome { get; private set; }
+        public Linha(string nome)
+        {
+            Nome = nome;
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Nome.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return Nome.GetHashCode();
+        }
+    }
+}
---
+++ refatoracao/R22.ReplaceArrayWithObject/antes/Empresa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace refatoracao.R22.ReplaceArrayWithObject.antes
+{
+    class Empresa
+    {
+        public Empresa()
+        {
+            string[] filial = new string[3];
+            filial[0] = "Curitiba";
+            filial[1] = "15";
+        }
+    }
+}
