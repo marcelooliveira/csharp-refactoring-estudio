13adad041d64c17df6023eac20669eba69c85dc7
alexfelipe <alexfelipevieira@gmail.com>
2017-11-01 14:26:20 -0200

    .
    .

---
+++ refatoracao/Aula04/R08.ReplaceMethodWithMethodObject/depois/Produto.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace refatoracao.R08.ReplaceMethodWithMethodObject.depois
+{
+    class Produto
+    {
+        private readonly string descricao;
+        private bool promocional;
+        private readonly double precoBase;
+        private readonly double acrescimo;
+        private readonly double desconto;
+
+        public string Descricao { get => descricao; }
+        public bool Promocional { get => promocional; }
+        public double PrecoBase { get => precoBase; }
+        public double Acrescimo { get => acrescimo; }
+        public double Desconto { get => desconto; }
+
+        public Produto(string descricao, double precoBase, double acrescimo, double desconto)
+        {
+            this.descricao = descricao;
+            this.precoBase = precoBase;
+            this.acrescimo = acrescimo;
+            this.desconto = desconto;
+
+            var preco = Preco(precoBase, acrescimo, desconto);
+
+            Console.WriteLine($"ANTES: O preço é {preco}");
+        }
+
+        double Preco(double precoBase, double acrescimo, double desconto)
+        {
+            //aqui viria um cálculo muito mais complicado do que esse...
+            return precoBase + acrescimo - desconto;
+        }
+
+        public void HabilitarPromocao()
+        {
+            if (desconto == 0)
+            {
+                this.promocional = true;
+            }
+            else
+            {
+                throw new Exception("Produto com desconto não pode ser transformado em produto promocional!");
+            }
+        }
+    }
+}
