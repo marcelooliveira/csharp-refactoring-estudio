d383fea04d7ccaccacf94a5437e6bd7554fd085c
alexfelipe <alexfelipevieira@gmail.com>
2017-11-01 15:14:12 -0200

    .
    .

--- refatoracao/Aula04/R08.ReplaceMethodWithMethodObject/depois/Produto.cs
+++ refatoracao/Aula04/R08.ReplaceMethodWithMethodObject/depois/Produto.cs
@@ -6,17 +6,17 @@ namespace refatoracao.R08.ReplaceMethodWithMethodObject.depois
     {
         private readonly string descricao;
         private bool promocional;
-        private readonly double precoBase;
-        private readonly double acrescimo;
-        private readonly double desconto;
+        private readonly decimal precoBase;
+        private readonly decimal acrescimo;
+        private readonly decimal desconto;
 
         public string Descricao { get => descricao; }
         public bool Promocional { get => promocional; }
-        public double PrecoBase { get => precoBase; }
-        public double Acrescimo { get => acrescimo; }
-        public double Desconto { get => desconto; }
+        public decimal PrecoBase { get => precoBase; }
+        public decimal Acrescimo { get => acrescimo; }
+        public decimal Desconto { get => desconto; }
 
-        public Produto(string descricao, double precoBase, double acrescimo, double desconto)
+        public Produto(string descricao, decimal precoBase, decimal acrescimo, decimal desconto)
         {
             this.descricao = descricao;
             this.precoBase = precoBase;
@@ -28,13 +28,13 @@ namespace refatoracao.R08.ReplaceMethodWithMethodObject.depois
             Console.WriteLine($"ANTES: O preço é {preco}");
         }
 
-        double Preco(double precoBase, double acrescimo, double desconto)
+        decimal Preco(decimal precoBase, decimal acrescimo, decimal desconto)
         {
             var resultado = precoBase;
 
-            if (acrescimo > 0 && desconto > 0)
+            if (this.promocional && desconto > 0)
             {
-                throw new Exception("Produto não pode ter acréscimo e desconto ao mesmo tempo!");
+                throw new Exception("Produto já é promocional e não pode ter desconto!");
             }
 
             if (desconto > 20)
